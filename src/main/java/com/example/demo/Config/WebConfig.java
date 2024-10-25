90027d42abde276a7a6e05baa958186a8de8c9bd
Utkarsh Meshram <utkarsh125msh@gmail.com>
2024-10-25 17:41:26 +0530

    Updated CORS configuration

--- src/main/java/com/example/demo/Config/WebConfig.java
+++ src/main/java/com/example/demo/Config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) { 
         registry.addMapping("/**") // Allows CORS for all endpoints
             .allowedOrigins("https://quiz-app2024.vercel.app") // Replace with your frontend URL
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
